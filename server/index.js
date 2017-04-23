4a4f6781209a7a99cbee7651c3dbb31a7a288c3f
Christopher Hummel <chrishummelj@gmail.com>
2017-04-23 17:51:14 -0400

    make intial front end page

--- server/index.js
+++ server/index.js
@@ -7,4 +7,10 @@ import config from './config'
 export const store = makeStore()
 startServer(store)
 
+store.dispath({
+  type: 'SET_ENTRIES',
+  entries: require('./entries/entries.json')
+})
+store.dispath({type: 'NEXT'})
+
 
